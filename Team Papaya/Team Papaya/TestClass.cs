983a7c707bf1d4a1e6051b67eab0d74788691903
Iliev88 <iliev.ilya@yahoo.com>
2017-04-16 18:18:56 +0300

    Creating POM for HomePage and some elementary tests

--- Team Papaya/Team Papaya/TestClass.cs
+++ Team Papaya/Team Papaya/TestClass.cs
@@ -1,20 +1,72 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Team_Papaya.Pages.HomePage;
 
 namespace Team_Papaya
 {
     [TestFixture]
     public class TestClass
     {
-        [Test]
-        public void TestMethod()
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
         {
-            // TODO: Add your test code here
-            Assert.Pass("Your first passing test");
+            driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+        
+        // SOME ELEMENTARY TESTS FOR HOMEPAGE
+        // MUST THINK HOW EVERYONE COULD NAVIGATE TO THE BLOG
+        [Test]
+        public void HomePageHeaderTest()
+        {
+            var homePage = new HomePage(driver);
+
+            homePage.NavigateTo();
+
+            homePage.AssertHomePageHeaderIsDisplayed("SOFTUNI BLOG");
+        }
+
+        [Test]
+        public void HomePageFooterTest()
+        {
+            var homePage = new HomePage(driver);
+
+            homePage.NavigateTo();
+
+            homePage.AssertHomePageFooterIsDisplayed("© 2017 - SoftUni Blog");
+        }
+
+        [Test]
+        public void HomePageRegisterButtonTest()
+        {
+            var homePage = new HomePage(driver);
+
+            homePage.NavigateTo();
+
+            homePage.AssertHomePageRegisterButtonIsDisplayed("Register");
+        }
+
+        [Test]
+        public void HomePageLoginrButtonTest()
+        {
+            var homePage = new HomePage(driver);
+
+            homePage.NavigateTo();
+
+            homePage.AssertHomePageLoginButtonIsDisplayed("Log in");
         }
     }
 }
