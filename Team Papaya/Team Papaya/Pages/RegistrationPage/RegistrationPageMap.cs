bc845f28a802896e885817f2fc6cb846ddd87d4c
Iliev88 <iliev.ilya@yahoo.com>
2017-04-19 08:43:25 +0300

    start building POM for Registration page

---
+++ Team Papaya/Team Papaya/Pages/RegistrationPage/RegistrationPageMap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_Papaya.Pages.RegistrationPage
+{
+    public partial class RegistrationPage
+    {
+    }
+}
