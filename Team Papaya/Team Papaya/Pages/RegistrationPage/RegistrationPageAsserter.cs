7f58b3b8f3719bddf2c6cbb2d39fafd821d496ef
Iliev88 <iliev.ilya@yahoo.com>
2017-04-19 15:30:46 +0300

    adding asserters for registration page

--- Team Papaya/Team Papaya/Pages/RegistrationPage/RegistrationPageAsserter.cs
+++ Team Papaya/Team Papaya/Pages/RegistrationPage/RegistrationPageAsserter.cs
@@ -14,5 +14,35 @@ namespace Team_Papaya.Pages.RegistrationPage
             Assert.IsTrue(page.ErrorMessageForMissingEmail.Displayed);
             Assert.AreEqual(text, page.ErrorMessageForMissingEmail.Text);
         }
+
+        public static void AssertInvalidEmailMessage(this RegistrationPage page, string text)
+        {
+            Assert.IsTrue(page.ErrorMessageForInvalidEmail.Displayed);
+            Assert.AreEqual(text, page.ErrorMessageForInvalidEmail.Text);
+        }
+
+        public static void AssertMissingFullNameMessage(this RegistrationPage page, string text)
+        {
+            Assert.IsTrue(page.ErrorMessageForMissingFullName.Displayed);
+            Assert.AreEqual(text, page.ErrorMessageForMissingFullName.Text);
+        }
+
+        public static void AssertTooLongFullNameMessage(this RegistrationPage page, string text)
+        {
+            Assert.IsTrue(page.ErrorMessageForTooLongFullName.Displayed);
+            Assert.AreEqual(text, page.ErrorMessageForTooLongFullName.Text);
+        }
+
+        public static void AssertMissingPasswordMessage(this RegistrationPage page, string text)
+        {
+            Assert.IsTrue(page.ErrorMessageForMissingPassword.Displayed);
+            Assert.AreEqual(text, page.ErrorMessageForMissingPassword.Text);
+        }
+
+        public static void AssertUnmatchedPasswordMessage(this RegistrationPage page, string text)
+        {
+            Assert.IsTrue(page.ErrorMessageForUnmatchedPasswords.Displayed);
+            Assert.AreEqual(text, page.ErrorMessageForUnmatchedPasswords.Text);
+        }
     }
 }
