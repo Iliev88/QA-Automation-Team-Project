e401fd176450d3adb1997b1fcb0eb788ee534a1c
PeUzunov <puzunovmail@gmail.com>
2017-05-14 12:45:39 +0300

    Added Create Article Page, Asserter and Map

---
+++ Team Papaya/Team Papaya/Pages/CreateArticlePage/CreateArticleAsserter.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_Papaya.Pages.CreateArticlePage
+{
+    public static class CreateArticleAsserter
+    {
+        public static void AssertRegisterWithValidData(this CreateArticlePage page)
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
