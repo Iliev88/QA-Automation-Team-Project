fe3a604b32a302f486f614fe65efd3cbd11565ea
PeUzunov <puzunovmail@gmail.com>
2017-05-20 11:15:27 +0300

    Added Delete Article Page, Asserter and Map

---
+++ Team Papaya/Team Papaya/Pages/DeleteArticlePage/DeleteArticlePage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_Papaya.Pages.DeleteArticlePage
+{
+    public partial class DeleteArticlePage : BasePage
+    {
+        public DeleteArticlePage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public void NavigateTo()
+        {
+            Driver.Navigate().GoToUrl(@"http://localhost:60064/Article/Delete");
+        }
+    }
+}
