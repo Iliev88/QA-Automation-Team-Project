3a9763ead85edab5c0ab3a43100795b6e163f442
PeUzunov <puzunovmail@gmail.com>
2017-06-08 20:11:25 +0300

    new TC installed trying to sync

--- Team Papaya/Blog/Controllers/Admin/UserController.cs
+++ Team Papaya/Blog/Controllers/Admin/UserController.cs
@@ -1,263 +1,306 @@
-﻿using Blog.Models;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Blog.Controllers.Admin
-{
-    [Authorize(Roles = "Admin")]
-    public class UserController : Controller
-    {
-        //
-        // GET: User
-        public ActionResult Index()
-        {
-            return RedirectToAction("List");
-        }
-
-        //
-        // GET: User/List
-        public ActionResult List()
-        {
-            using (var database = new BlogDbContext())
-            {
-                var users = database.Users
-                    .ToList();
-
-                var admins = GetAdminUserNames(users, database);
-                ViewBag.Admins = admins;
-
-                return View(users);
-            }
-        }
-
-        //
-        // GET: User/Edit
-        public ActionResult Edit(string id)
-        {
-            // Validate Id
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            using (var database = new BlogDbContext())
-            {
-                // Get user from database
-                var user = database.Users
-                    .Where(u => u.Id == id)
-                    .First();
-
-                // Check if user exists
-                if (user == null)
-                {
-                    return HttpNotFound();
-                }
-
-                // Create a view model
-                var viewModel = new EditUserViewModel();
-                viewModel.User = user;
-                viewModel.Roles = GetUserRoles(user, database);
-
-                // Pass the model to the view
-                return View(viewModel);
-            }
-        }
-
-        //
-        // POST: User/Edit
-        [HttpPost]
-        public ActionResult Edit(string id, EditUserViewModel viewModel)
-        {
-            if (ModelState.IsValid)
-            {
-                using (var db = new BlogDbContext())
-                {
-                    var user = db.Users.FirstOrDefault(u => u.Id.Equals(id));
-
-                    if (user == null)
-                    {
-                        //
-                    }
-
-                    user.Email = viewModel.User.Email;
-                    user.FullName = viewModel.User.FullName;
-                    user.UserName = viewModel.User.Email;
-
-                    if (!string.IsNullOrEmpty(viewModel.Password))
-                    {
-                        var passwordHasher = new PasswordHasher();
-                        var newPasswordHash = passwordHasher.HashPassword(viewModel.Password);
-                        user.PasswordHash = newPasswordHash;
-                    }
-
-                    SetUserRoles(user, db, viewModel);
-
-                    db.Entry(user).State = EntityState.Modified;
-                    db.SaveChanges();
-                }
-
-                return RedirectToAction("List");
-            }
-
-            return View(viewModel);
-        }
-
-        //
-        // GET: User/Delete
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            using (var database = new BlogDbContext())
-            {
-                // Get user from database
-                var user = database.Users
-                    .Where(u => u.Id.Equals(id))
-                    .First();
-
-                // Check if user exists
-                if (user == null)
-                {
-                    return HttpNotFound();
-                }
-
-                return View(user);
-            }
-        }
-
-        //
-        // POST: User/Delete
-        [HttpPost]
-        [ActionName("Delete")]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            using (var database = new BlogDbContext())
-            {
-                // Get user from database
-                var user = database.Users
-                    .Where(u => u.Id.Equals(id))
-                    .First();
-
-                // Get user articles from database
-                var userArticles = database.Articles
-                    .Where(a => a.Author.Id == user.Id);
-
-                // Delete user articles
-                foreach (var article in userArticles)
-                {
-                    database.Articles.Remove(article);
-                }
-
-                // Delete user and save changes
-                database.Users.Remove(user);
-                database.SaveChanges();
-
-                return RedirectToAction("List");
-            }
-        }
-
-        private HashSet<string> GetAdminUserNames(List<ApplicationUser> users, BlogDbContext context)
-        {
-            var admins = new HashSet<string>();
-
-            var userManager = new UserManager<ApplicationUser>(
-                new UserStore<ApplicationUser>(context));
-
-            foreach (var user in users)
-            {
-                if (userManager.IsInRole(user.Id, "Admin"))
-                {
-                    admins.Add(user.UserName);
-                }
-            }
-
-            return admins;
-        }
-
-        private void SetUserRoles(ApplicationUser user, BlogDbContext db, EditUserViewModel model)
-        {
-            var userManager = Request
-                .GetOwinContext()
-                .GetUserManager<ApplicationUserManager>();
-
-            foreach (var role in model.Roles)
-            {
-                if (role.IsSelected)
-                {
-                    userManager.AddToRole(user.Id, role.Name);
-                }
-                else if (!role.IsSelected)
-                {
-                    userManager.RemoveFromRole(user.Id, role.Name);
-                }
-            }
-        }
-
-        private void ChangeUserPassword(string userId, EditUserViewModel viewModel)
-        {
-            // Create user manager
-            var userManager = Request.GetOwinContext()
-                .GetUserManager<ApplicationUserManager>();
-
-            // Create password reset token
-            var token = userManager.GeneratePasswordResetToken(userId);
-            var result = userManager.ResetPassword(userId, token, viewModel.Password);
-
-            // Check if operation succeeded
-            if (!result.Succeeded)
-            {
-                throw new Exception(string.Join(";", result.Errors));
-            }
-        }
-
-        private List<Role> GetUserRoles(ApplicationUser user, BlogDbContext context)
-        {
-            // Create user and role managers
-            var userManager = new UserManager<ApplicationUser>(
-                new UserStore<ApplicationUser>(context));
-
-            var roleManager = new RoleManager<IdentityRole>(
-                new RoleStore<IdentityRole>(context));
-
-            // Get all application roles
-            var roles = roleManager.Roles
-                .Select(r => r.Name)
-                .OrderBy(r => r)
-                .ToList();
-
-            // For each application role, check if the user has it
-            var userRoles = new List<Role>();
-
-            foreach (var roleName in roles)
-            {
-                var role = new Role { Name = roleName };
-
-                if (userManager.IsInRole(user.Id, roleName))
-                {
-                    role.IsSelected = true;
-                }
-
-                userRoles.Add(role);
-            }
-
-            // Return a list with all roles
-            return userRoles;
-        }
-    }
+﻿<font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font rwr="1" style="font-family:Arial" size="4"><font size="3" face="Georgia">
+</font><style type="text/css">
+#SuperBG {
+width:100%;
+background-image:url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/BG.jpg');
 }
+#SuperWrapper {
+width: 750px;
+margin-left: auto;
+margin-right: auto;
+font-family: arial, Helvetica, sans-serif;
+font-size: 12px;
+color: #000000;
+}
+#SuperWrapper p {
+margin: 0px;
+padding: 0px 0px 15px 0px;
+line-height: 20px;
+}
+#SuperWrapper h1 {
+padding: 0px 0px 10px 0px;
+margin: 0px;
+font-size: 30px;
+font-weight: bold;
+color: #000000;
+}
+#SuperWrapper a {
+font-weight: bold;
+text-decoration: underline;
+color: #000000;
+}
+#SuperWrapper a:hover {
+text-decoration:none;
+}
+#SuperHeader {
+width: 750px;
+height: 318px;
+background-image: url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/Header.jpg');
+}
+#SuperHeaderLogo {
+padding: 107px 0px 0px 0px;
+font-family: impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+font-size: 65px;
+letter-spacing: -1px;
+font-weight: normal;
+margin: 0px;
+text-align: center;
+line-height: 50px;
+color: #FFFFFF;
+text-shadow: -2px -3px -3px #000;
+text-transform: uppercase;
+}
+#SuperHeaderMenu {
+margin: 0px;
+}
+#SuperHeaderMenu ul.navi{
+padding: 0px;
+margin: 0px 0px 0px 0px;
+width: 750px;
+text-align: center;
+position: relative;
+}
+#SuperHeaderMenu ul.navi li{
+height: 22px;
+padding: 0 10px 0 10px;
+margin: 0px;
+display: inline;
+}
+#SuperHeaderMenu ul.navi li a{
+padding: 0px 8px 0px 8px;
+font: 18px arial, Helvetica, sans-serif;
+color: #FFFFFF;
+text-decoration: none;
+text-indent: 0px;
+font-weight: bold;
+margin: 0;
+width: inherit;
+letter-spacing: -1px;
+line-height: 30px;
+}
+#SuperHeaderMenu ul.navi li a:hover{
+color: #FFFF99;
+}
+#SuperContentsWrapper {
+width: 750px;
+background-image: url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/Contents.jpg');
+}
+#SuperContents {
+width: 750px;
+background-image: url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/Contents.jpg');
+background-repeat: no-repeat;
+}
+#SuperContentsSub {
+padding: 0px 40px 0px 40px;
+}
+#SuperFooter {
+width: 750px;
+height: 44px;
+background-image:url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/Footer.jpg');
+}
+#SuperFooterLink {
+width: 750px;
+background-image:url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/BG.jpg');
+height: 100px;
+}
+#SuperBoxContents {
+padding: 0px 80px 0px 80px;
+margin: 0px;
+color: #000000;
+}
+#SuperBoxContents p {
+padding: 0px 0px 10px 0px;
+margin: 0px;
+line-height: 20px;
+}
+#SuperBoxContents a {
+color: #000000;
+text-decoration: underline;
+}
+#SuperBoxContents a:hover {
+text-decoration:none;
+}
+#SuperBoxContents ul {
+padding: 0px 0px 0px 28px;
+margin: 0px;
+list-style-type: disc;
+}
+#SuperBoxContents li {
+line-height: 20px;
+}
+#SuperPayment {
+width: 750px;
+}
+#SuperPaymentTop {
+width: 750px;
+height: 83px;
+background-image:url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/PaymentPolicyTop.jpg');
+}
+#SuperPaymentContents {
+width: 750px;
+background-image:url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/PaymentPolicyContents.jpg');
+}
+#SuperPaymentBottom {
+width: 750px;
+height: 53px;
+background-image:url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/PaymentPolicyBottom.jpg');
+}
+#SuperShipping {
+width: 750px;
+}
+#SuperShippingTop {
+width: 750px;
+height: 83px;
+background-image: url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/ShippingPolicyTop.jpg');
+}
+#SuperShippingContents {
+width: 750px;
+background-image:url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/ShippingPolicyContents.jpg');
+}
+#SuperShippingBottom {
+width: 750px;
+height: 53px;
+background-image:url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/ShippingPolicyBottom.jpg');
+}
+#SuperContacts {
+width: 750px;
+}
+#SuperContactsTop {
+width: 750px;
+height: 83px;
+background-image:url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/ContactsTop.jpg');
+}
+#SuperContactsContents {
+width: 750px;
+background-image:url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/ContactsContents.jpg');
+}
+#SuperContactsBottom {
+width: 750px;
+height: 53px;
+background-image:url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/ContactsBottom.jpg');
+}
+#SuperReturns {
+width: 750px;
+}
+#SuperReturnsTop {
+width: 750px;
+height: 83px;
+background-image:url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/ReturnsTop.jpg');
+}
+#SuperReturnsContents {
+width: 750px;
+background-image:url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/ReturnsContents.jpg');
+}
+#SuperReturnsBottom {
+width: 750px;
+height: 53px;
+background-image:url('http://www.superauctiontemplate.com/freebaytemplate/productimages/GeneralShopping16/HTML/images/ReturnsBottom.jpg');
+}
+/* HTML5 ELEMENTS */
+/* sub images > thumbnail list */
+/* sub images > mouse over */
+ul#SuperThumbs a:hover {
+overflow: visible;
+z-index: 1000;
+border: none;
+}
+/* sub images > clearing floats */
+ul#SuperThumbs:after, li#SuperThumbs:after {
+content: ".";
+display: block;
+height: 0;
+clear: both;
+visibility: hidden;
+}
+ul#SuperThumbs, li#SuperThumbs {
+display: block;
+}
+ul#SuperThumbs, li#SuperThumbs {
+min-height: 1%;
+}
+* html ul#SuperThumbs, * html li#SuperThumbs {
+height: 1%;
+}
+</style><div id="SuperBG">
+<div id="SuperWrapper">
+<div id="SuperHeader">
+<div id="SuperHeaderLogo"><b><font size="6" face="Georgia">smart-uk-bargains</font></b></div>
+<div id="SuperHeaderMenu">
+</div>
+</div>
+<div id="SuperContentsWrapper">
+<div id="SuperContents">
+<div id="SuperContentsSub">
+<h1 align="center"><font size="3" face="Georgia">
+ Tech Armor - Matte Anti-Glare/Anti-Fingerprint Film Screen Protector for Apple iPhone 6S / iPhone 6 (4.7 inch) [3-Pack] 
+</font></h1>
+<p><font size="3" face="Georgia">Item description:</font></p>
+<ul>
+<li><font size="3" face="Georgia">   Compatible Model(s): iPhone 6S / iPhone 6 (4.7 inch) by Apple [3 Pack] [PLEASE NOTE: Matte Antiglare may reduce display clarity][PLEASE NOTE: High Definition AntiGlare products use Matte technology with a glossy finish]   </font></li>
+<li><font size="3" face="Georgia">  Tech Armor AntiGlare Screen Protectors are matte finished to remove glare while an Oleophobic Coating magically resists oils and reduces annoying fingerprints.   </font></li>
+<li><font size="3" face="Georgia">     Tech Armor Antiglare is the affordable way to protect your iPhone 6S/6 screen from scratches and drops. Only premium materials and expert design go into this Best In Class product. </font></li>
+<li><font size="3" face="Georgia">     Tech Armor is THE trusted online resource for screen protection.   </font></li>
+<li><font size="3" face="Georgia">    No Bubble, easy install, and perfect fit.    </font></li>
+</ul>
+</div>
+</div>
+<div id="SuperPayment">
+<div id="SuperPaymentContents">
+<div id="SuperBoxContents">
+<p><font size="3" face="Georgia">
+<b>Payment Policy
+</b></font></p>
+<ul>
+<li><font size="3" face="Georgia">For more secure transactions and better customer satisfaction we accept only PayPal payment.</font></li>
+<li><font size="3" face="Georgia">You do not need a PayPal account, you can pay with your credit or debit card via PayPal.</font></li><br>
+</ul>
+</div>
+</div>
+</div>
+<div id="SuperShipping">
+<div id="SuperShippingContents">
+<div id="SuperBoxContents">
+<p><font size="3" face="Georgia">
+<b>Shipping
+</b></font></p>
+<ul>
+<li><font size="3" face="Georgia">Our standard shipping service is free, your order will be processed within 1-2 business days after your payment.</font></li>
+<li><font size="3" face="Georgia">Free shipping to mainland UK.</font></li>
+<li><font size="3" face="Georgia">Shipping only to mainland UK.</font></li><br>
+</ul>
+</div>
+</div>
+</div>
+<div id="SuperContacts">
+<div id="SuperContactsContents">
+<div id="SuperBoxContents">
+<p><font size="3" face="Georgia">
+<b>Return Policy</b>
+</font></p>
+<ul>
+<li><font size="3" face="Georgia">All returns accepted.</font></li>
+<li><font size="3" face="Georgia">Buyer should contact within 30 Days after receiving the item.</font></li>
+<li><font size="3" face="Georgia">Refund given as Money Back.</font></li>
+<li><font size="3" face="Georgia">Return shipping will be paid by buyer.</font></li>
+<li><font size="3" face="Georgia">No restocking fee will be charged.</font></li>
+</ul>
+</div>
+</div>
+</div>
+<div id="SuperReturns">
+<div id="SuperReturnsContents">
+<div id="SuperBoxContents">
+<br>
+<p align="center"><font size="3" color="#002CFD" face="Georgia">HAVE A NICE DAY!<br>We are here to Serve you. Thank you for looking at our listing!
+<br>
+You're welcome to WATCH other items we sell. If you have any questions please feel free to ask We're always happy to answer your queries.</font></p>
+<h2><font size="3" face="Georgia">About us</font></h2>
+<p><font size="3" face="Georgia">All of our products ship directly from our different warehouses across the United Kingdom so you get your items quickly and in perfect condition. We know how important it is for you to get what you ordered and get it fast. Our products are a 100% authentic and brand new. If there's a problem, just let us know and we fix it. We guarantee you only the best experience when shopping with us!
+</font></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div><font size="3" face="Georgia">
+</font></font></font></font></font></font></font></font></font></font></font></font></font></font></font></font></font>
